3ac1bd7005db58dfcff7eb2f2dba434139a6daea
Josebias Neto <josebiasbsneto@gmail.com>
2021-07-28 21:32:24 -0300

    Set contacts list on recycle view

--- app/src/main/java/com/example/contatosbootcamp/MainActivity.kt
+++ app/src/main/java/com/example/contatosbootcamp/MainActivity.kt
@@ -4,11 +4,15 @@ import android.Manifest
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     val REQUEST_CONTACT = 1
+    val LINEAR_LAYOUT_VERTICAL = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +38,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setContacts() {
-        TODO("Not yet implemented")
+        val contactList: ArrayList<Contact> = ArrayList()
+        val cursor = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+        null, null, null, null)
+        if (cursor != null) {
+            while (cursor.moveToNext()){
+                contactList.add(Contact(
+                    cursor.getString(cursor.getColumnIndex(
+                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)),
+                    cursor.getString(cursor.getColumnIndex(
+                    ContactsContract.CommonDataKinds.Phone.NUMBER))
+                ))
+            }
+            cursor.close()
+        }
+
+        val adapter = ContactsAdapter(contactList)
+        val contactRecycleView = findViewById<RecyclerView>(R.id.rv_contacts)
+
+        contactRecycleView.layoutManager = LinearLayoutManager(this,
+            LINEAR_LAYOUT_VERTICAL,
+            false
+        )
+        contactRecycleView.adapter = adapter
     }
 }
